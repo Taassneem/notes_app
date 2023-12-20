d48f5cb8a7d2698c8e7caffd160ecb58b264cbff
Taassneem <tasnemeadwan87@gmail.com>
2023-12-20 09:56:33 +0000

    create note list view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/note_list_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -6,13 +7,31 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Notes',
-          style: TextStyle(fontSize: 24),
+        appBar: AppBar(
+          title: const Text(
+            'Notes',
+            style: TextStyle(fontSize: 24),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Container(
+                width: 45,
+                height: 45,
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.search,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
-        actions: const [Icon(Icons.search)],
-      ),
-    );
+        body: const NoteListView());
   }
 }
---
+++ lib/widgets/note_list_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/note.dart';
+
+class NoteListView extends StatelessWidget {
+  const NoteListView({super.key});
+final List<Color> colors =const[
+  Color(0xffD8A4DD),
+  Color(0xffFFCD7B),
+  Color(0xffE8E897),
+  Color(0xff77D6F1),
+  Color(0xffFFCD7B),
+  Color(0xffD8A4DD),
+  Color(0xffFFCD7B),
+  Color(0xff77D6F1),
+];
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return  NoteCard(color: colors[index],);
+        },
+      );
+  }
+}
